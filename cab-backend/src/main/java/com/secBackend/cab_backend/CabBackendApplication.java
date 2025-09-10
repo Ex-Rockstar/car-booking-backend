c6fb4b75e0bb85f2e19c95e35ec85dcc0856e380
Prasanna Harish <prasannaharish301@gmail.com>
2025-09-10 13:09:24 +0530

    springio generated

---
+++ cab-backend/src/main/java/com/secBackend/cab_backend/CabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.secBackend.cab_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBackendApplication.class, args);
+	}
+
+}
